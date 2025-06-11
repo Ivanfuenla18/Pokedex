19b756720d56b36d5318005670a8c9b20a899c32
ivanfuenla18 <ivanfuenla18@gmail.com>
2025-06-11 16:31:15 +0200

    First commit: Pokedex project

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+// src/main.jsx (o index.jsx)
 import { StrictMode } from "react";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
@@ -7,7 +8,9 @@ import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter basename="/Pokedex">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "@tailwindcss/vite"; // Asegúrate de que esto también esté aquí
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "/Pokedex/",
+  plugins: [
+    react(),
+    tailwindcss(), // Asegúrate de que esto también esté aquí
+  ],
+  base: "/Pokedex/", // ¡Es CRUCIAL que sea el nombre de tu repositorio con barras!
 });
